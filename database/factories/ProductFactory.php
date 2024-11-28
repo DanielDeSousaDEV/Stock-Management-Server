4e322d7b53e123161ae31ab9731a41a77efa972c
DanielDeSousaDEV <danieldesousa.dev@gmail.com>
2024-11-28 10:58:27 -0300

    factory do produto

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Products;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'description' => fake()->paragraph(2),
+            'price' => fake()->randomFloat(2, 10, 40),
+            'quantity' => fake()->numberBetween(30, 40),
+            'minimum_quantity' => fake()->numberBetween(50, 60),
+        ];
+    }
+}
