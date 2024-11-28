8dde03d473ae7aff1cb1dfe6040e87e7e4bb0db5
DanielDeSousaDEV <danieldesousa.dev@gmail.com>
2024-11-28 10:19:33 -0300

    wip

--- app/Models/StockMovements.php
+++ app/Models/StockMovements.php
@@ -3,8 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class StockMovements extends Model
 {
-    
+    protected $fillable = [
+        'products_id',
+        'locations_id',
+        'quantity',
+        'type',
+        'reason',
+    ];
+
+    protected function product (): HasOne {
+        return $this->hasOne(Products::class);
+    }
+
+    protected function location (): HasOne {
+        return $this->hasOne(Locations::class);
+    }
 }
